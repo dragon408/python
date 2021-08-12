bf0060abbdb83a4c836ce6006cc6b670d1634d20
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2021-08-12 16:15:10 +0300

    Add files via upload

---
+++ set/frozensett.py
@@ -0,0 +1,7 @@
+a = set('qwerty')
+b = frozenset('qwerty')
+print(a == b)
+
+#The only difference between set and frozenset is that set is a mutable datatype, but frozenset is not.
+
+#Единственное отличие set от frozenset заключается в том, что set - изменяемый тип данных, а frozenset - нет
