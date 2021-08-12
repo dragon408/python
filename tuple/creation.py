66978571e31d5063aecb09b025dbe1be1ce4b23e
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2021-08-12 16:17:08 +0300

    Create creation.py

---
+++ tuple/creation.py
@@ -0,0 +1,19 @@
+def orr():
+    print("---------------------------------------------")
+
+a = tuple()
+print(a)
+
+
+orr()
+
+
+a = ('s', )
+print(a)
+
+
+orr()
+
+
+a = tuple('hello, world!')
+print(a)
