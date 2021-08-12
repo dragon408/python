bd2dd0476ad1f62b88d51a5e63b400c1c7e0cc85
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2021-08-12 16:14:49 +0300

    Create sett.py

---
+++ set/sett.py
@@ -0,0 +1,19 @@
+def orr():
+    print("---------------------------------------------")
+
+a = set()
+print(a)
+
+
+orr()
+
+
+a = set('hello')
+print(a)
+
+
+orr()
+
+
+a = {i ** 2 for i in range(10)}
+print(a)
