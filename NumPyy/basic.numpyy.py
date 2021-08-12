4409ddb715e25463a0b50ac70fef3a03d8644341
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2021-08-12 16:09:00 +0300

    Create basic.numpyy.py

---
+++ NumPyy/basic.numpyy.py
@@ -0,0 +1,9 @@
+import numpy as np #library import
+
+a = np.array([[1, 2, 3],
+             [12, 23, 34]])#creating an array
+print(a) #array output
+
+#usually this library is used for matrices,
+#but with such arrays you can use the same
+#bases and sorts as with ordinary ones
