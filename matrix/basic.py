cc99ae8f7af75fb78dbd67ed62ca9cead09ce132
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2021-08-12 16:05:57 +0300

    Create basic.py

---
+++ matrix/basic.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+matrix = np.array([[1, 3, 5],[2, 4, 6]], int)
+
+print(matrix)
+
+size = matrix.shape
+print(size)
+
+temp = matrix[0]
+print(temp)
+
+
+arr = np.array([[3,3,3],[2,5,5]], str)
+temp = arr[1][2]
+print(temp)
